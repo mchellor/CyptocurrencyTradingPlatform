d3537484e233acfc0354f0bdbdd5cafeda74129d
mchellor <mc.hellor@gmail.com>
2019-06-06 15:35:54 -0400

    push my branch

--- Trading/src/main/java/com/testclass/Main.java
+++ Trading/src/main/java/com/testclass/Main.java
@@ -2,7 +2,13 @@ package com.testclass;
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("My Third Test in Eclipse");
+		System.out.println("My Fourth Test in Eclipse");
+		System.out.println("modified by Can Li");
+		System.out.println("GC test1");
+		System.out.println("new test");
+		System.out.println("Modefy in Web Client!!!");
+		
 		System.out.println("This is GC's Branch");
+		System.out.println("Try to merge my branch to master");
 	}
 }
