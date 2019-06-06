52ec03d74d134e49a098747a7c001fcdbe212c9e
lican <can.li.rutgers@gmail.com>
2019-06-06 14:57:58 -0400

    Can modified test

--- Trading/src/main/java/com/testclass/Main.java
+++ Trading/src/main/java/com/testclass/Main.java
@@ -3,5 +3,6 @@ package com.testclass;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("My Fourth Test in Eclipse");
+		System.out.println("modified by Can Li");
 	}
 }
