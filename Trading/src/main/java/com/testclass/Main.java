179e3b4d5a39ca0fd5698ad7c3075d4290b04a7b
mchellor <mc.hellor@gmail.com>
2019-06-06 14:37:58 -0400

    GC Branch push test

--- Trading/src/main/java/com/testclass/Main.java
+++ Trading/src/main/java/com/testclass/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("My Fourth Test in Eclipse");
 		System.out.println("modified by Can Li");
+		System.out.println("GC test1");
 	}
 }
