541fef014da7c45486bacd7dbeae5b6acbcf9631
mchellor <mc.hellor@gmail.com>
2019-06-06 14:37:58 -0400

    GC Branch push test

--- Trading/src/main/java/com/testclass/Main.java
+++ Trading/src/main/java/com/testclass/Main.java
@@ -3,5 +3,6 @@ package com.testclass;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("My Third Test in Eclipse");
+		System.out.println("This is GC's Branch");
 	}
 }
