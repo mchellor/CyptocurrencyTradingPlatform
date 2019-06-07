c0fa07ca49b1b2f149a822a8a1b24f8ac3c4dedb
lican <can.li.rutgers@gmail.com>
2019-06-07 15:52:18 -0400

    init

--- Trading/src/main/java/com/testclass/Main.java
+++ Trading/src/main/java/com/testclass/Main.java
@@ -1,24 +1,7 @@
 package com.testclass;
 
 public class Main {
-	public static void main(String[] args){
-		System.out.println("My Fourth Test in Eclipse");
-		System.out.println("modified by Can Li");
-		System.out.println("GC test1");
-		System.out.println("new test");
-		System.out.println("Modefy in Web Client!!!");
-		
-		System.out.println("This is GC's Branch");
-		System.out.println("Try to merge my branch to master");
-		System.out.println("Second Try....");
-		
-		System.out.println("My Third Try...");
-		System.out.println("Friday fourth");
-		System.out.println("fri 2:48");
-		System.out.println("Fri 3:07");
-		System.out.println("3:22");
-		
-		System.out.println("3:32");
-		System.out.println("3:40");
+	public static void main(String[] args) {
+		System.out.println("3:52");
 	}
 }
