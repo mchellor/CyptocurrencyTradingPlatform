b2ad0bfef7167d2cdd95f27b9838ae17ed296b0d
lican <can.li.rutgers@gmail.com>
2019-06-07 22:43:10 -0400

    Junit

--- Trading/src/main/java/com/traderjoey/utils/DBUtil.java
+++ Trading/src/main/java/com/traderjoey/utils/DBUtil.java
@@ -1,5 +1,33 @@
 package com.traderjoey.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import com.traderjoey.exceptions.SqlConnectionFailException;
+
 public class DBUtil {
+	private String driverString = "com.mysql.jdbc.Driver";
+	private String urlString = "jdbc:mysql://localhost:3306/traderjoey";
+	private String userString = "root";
+	private String passwordString = "admin";
+	private Connection connection = null;
+
+	@Test
+	public Connection getConnection() {
+		try {
+			Class.forName(driverString);
+			connection = DriverManager.getConnection(urlString, userString, passwordString);
+			connection.setAutoCommit(false);
+			if (connection == null) {
+				throw new SqlConnectionFailException();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return connection;
+	}
 
 }
